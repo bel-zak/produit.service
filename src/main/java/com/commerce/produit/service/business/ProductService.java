3a35fec63a2304d471b57a8c07c0a8ff3a1af0d5
Zakaria BELHISSI <belhissi.zakaria@gmail.com>
2024-08-05 16:59:19 +0200

    Add Product entity,service  and repository

--- src/main/java/com/commerce/produit/service/business/ProductService.java
+++ src/main/java/com/commerce/produit/service/business/ProductService.java
@@ -7,5 +7,28 @@ import jakarta.persistence.Id;
 
 @Entity
 public class ProductService {
+ private Long id;
+   private String name;
+   private int stockQuantity;
+   private double price;
 
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "ProductService{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", stockQuantity=" + stockQuantity +
+                ", price=" + price +
+                '}';
+    }
 }
--- src/main/java/com/commerce/produit/service/repository/ProductRepository.java
+++ src/main/java/com/commerce/produit/service/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.commerce.produit.service.repository;
 
-public class ProductRepository {
+import com.commerce.produit.service.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product,Long> {
 }
